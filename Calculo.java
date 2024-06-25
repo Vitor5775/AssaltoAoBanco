557fc572b2a1bba02a5de945c831d67ac3d99785
VitorRusqui <164022766+Vitor5775@users.noreply.github.com>
2024-06-25 03:01:35 -0300

    Update Calculo.java

--- Calculo.java
+++ Calculo.java
@@ -1,13 +1,22 @@
-public abstract class Calculo {
-    public float resultado;
-    public Calculo() {
-        this.resultado = 0;
-    }
-    public abstract float obtResultado(float[] _valReceitaBruta);
-    public float getValResultado() {
-        return resultado;
-    }
-    public void setValResultado(float resultado) {
-        this.resultado = resultado;
+import java.util.Scanner;
+
+public class BancoAssalto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] dinheiroPessoas = new int[10];
+        int totalRoubado = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite a quantidade de dinheiro da pessoa " + (i + 1) + ": ");
+            dinheiroPessoas[i] = scanner.nextInt();
+        } 
+        System.out.print("Digite a quantidade de dinheiro que o bandido confesso revelou: ");
+        int dinheiroBandidoConfesso = scanner.nextInt();
+        for (int valor : dinheiroPessoas) {
+            if (valor % 10 == 0 && valor >= dinheiroBandidoConfesso) {
+                totalRoubado += valor;
+            }
+        }
+
+        System.out.println("O total roubado foi: " + totalRoubado + " reais");
     }
 }
